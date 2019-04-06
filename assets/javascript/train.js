51ee4594b70293c7a66513683ad508906dc00225
Calypso Blue <46878997+Avalineai@users.noreply.github.com>
2019-04-06 12:38:03 -0700

    time calculation revision

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,3 +1,5 @@
+//user interval to deduct time
+
 
   // Initialize Firebase
 var config = {
@@ -21,18 +23,24 @@ $("#submit-info").on("click", function (event) {
     var firstTrain = $("#first-train").val().trim();
     var frequency = $("#frequency").val().trim()
     console.log(frequency)
-
     console.log(trainName)
     console.log(firstTrain)
 
+    // if (typeof firstTrain === ("string")) {
+    //     alert("input invalid")
+    // }
 
+    var currentTime = moment().format("HH:mm")
+    console.log("CURRENT TIME: " + currentTime)
 
-    var currentTime = moment()
-    console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm"))
     var convertFirstTime = moment(firstTrain, "HH:mm").subtract(1, "years")
     console.log("First Time Converted " + convertFirstTime)
-    var diffInTime = moment().diff(moment(convertFirstTime), "minutes")
+
+    var diffInTime = moment(currentTime).format("HH:mm").diff(moment(convertFirstTime), "minutes")
+    console.log(moment())
+    console.log((moment(convertFirstTime)))
     console.log("First Train Time in Minutes " + diffInTime)
+
     var minutesTill = frequency - (diffInTime % frequency)
     console.log("Minutes Away " + minutesTill)
     var nextTrain = moment().add(minutesTill, "minutes")
@@ -58,9 +66,6 @@ database.ref("/TrainInfo").on("child_added", function (snapshot) {
     var addRow = $("<tr>")
     var trainTd = $("<td>").text(snapshot.val().trainName)
     var destTd = $("<td>").text(snapshot.val().destination)
-
-    //var firstTrainTd = $("<td>").text(snapshot.val().firstTrain)
-
     var frequencyTd = $("<td>").text(snapshot.val().frequency)
     var nextArrival = $("<td>").text(snapshot.val().nextTrainTime)
     var minutesAwayTd = $("<td>").text(snapshot.val().minutesTill)
@@ -68,4 +73,48 @@ database.ref("/TrainInfo").on("child_added", function (snapshot) {
     addRow.append(trainTd, destTd, frequencyTd, nextArrival, minutesAwayTd)
     $("#train-info").append(addRow)
 
+
+}, function(errorObject) {
+    console.log("The read failed: " + errorObject.code);
+    
 })
+
+function timeCatch() {
+
+
+}
+
+// function validateTime(obj) {
+//     var timeValue = obj;
+//     if (timeValue == "" || timeValue.indexOf(":") < 0) {
+//         alert("Invalid Time format");
+//         return false;
+//     }
+//     else {
+//         var sHours = timeValue.split(':')[0];
+//         var sMinutes = timeValue.split(':')[1];
+
+//         if (sHours == "" || isNaN(sHours) || parseInt(sHours) > 23) {
+//             alert("Invalid Time format");
+//             return false;
+//         }
+//         else if (parseInt(sHours) == 0)
+//             sHours = "00";
+//         else if (sHours < 10)
+//             sHours = "0" + sHours;
+
+//         if (sMinutes == "" || isNaN(sMinutes) || parseInt(sMinutes) > 59) {
+//             alert("Invalid Time format");
+//             return false;
+//         }
+//         else if (parseInt(sMinutes) == 0)
+//             sMinutes = "00";
+//         else if (sMinutes < 10)
+//             sMinutes = "0" + sMinutes;
+
+//         obj = sHours + ":" + sMinutes;
+//     }
+
+//     return true;
+//  }
+
